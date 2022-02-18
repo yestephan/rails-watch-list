cea9dcac9d8c3d5479831e661c21a73d5e5a22db
stephan ye <ye.stephan@gmail.com>
2022-02-18 13:04:40 +0100

    updated bookmarks destroy dependency

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
-  has_many :bookmarks
+  has_many :bookmarks, dependent: :destroy
   has_many :lists, through: :bookmarks
   validates :title, uniqueness: true, presence: true
   validates :overview, presence: true
